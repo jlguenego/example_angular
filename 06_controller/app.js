2dcbe89b2f5562c366480ec7f50615b033bbd867
Jean-Louis GUENEGO <jlguenego@gmail.com>
2014-10-07 14:38:44 +0200

    exo

---
+++ 06_controller/app.js
@@ -0,0 +1,13 @@
+(function() {
+	var someone = {
+		lastname: 'DUPOND',
+		firstname: 'Marcel',
+		age: 35
+	};
+
+	var app = angular.module('myApp', []);
+
+	app.controller('MyController', function() {
+		this.person = someone;
+	});
+})();
