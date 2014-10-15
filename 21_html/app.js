0099da2a1353a571f062d5d364c1e0c637a5fd53
Jean-Louis GUENEGO <jlguenego@gmail.com>
2014-10-15 15:51:53 +0200

    q

--- 21_html/app.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var app = angular.module('myApp', []);
-
-	app.controller('MyController', [ '$scope', '$http', function($scope, $http) {
-		$http.get('content.json').success(function(data) {
-			$scope.content = data.content;
-		});
-	}]);
-})();
