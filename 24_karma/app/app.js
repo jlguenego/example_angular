67c220454cf6550bd6e2cb3fa871218ea03c9b5a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2014-10-16 10:48:11 +0200

    q

--- 24_karma/app/app.js
+++ 24_karma/app/app.js
@@ -1,13 +1,7 @@
 (function() {
 	var app = angular.module('myApp', []);
 
-	app.factory('helloService', function() {
-		var result = {};
-		result.sayHello = function() { return "Hello World!" };
-		return result;
-	});
-
-	app.controller('myController', [ 'helloService', function(helloService) {
-		this.message = helloService.sayHello();
+	app.controller('myController', [ function() {
+		this.message = "Hello World!";
 	}]);
 })();
