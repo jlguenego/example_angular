bea75533ab7b55db6dfd94d9846ca60da102aeb8
Juan Trochez <juan.trochez95@gmail.com>
2015-02-27 11:56:52 +0100

    gra

--- 23_promise/app.js
+++ 23_promise/app.js
@@ -8,54 +8,49 @@
 	var k = 0.005;
 
 	var Particule = function(element, masse, vitesse, angle, x, y) {
+		var self = this;
 		this.masse = masse;
 		this.vx = vitesse * Math.cos(angle * 2 * Math.PI / 360);
 		this.vy = vitesse * Math.sin(angle * 2 * Math.PI / 360);
 		this.x = x;
 		this.y = y;
 		this.element = element;
-		this.move = function() {
+		this.move = function(next) {
 			this.vx += (Fx / this.masse) * (dt / 1000) - (this.vx * k);
 			this.vy += (Fy / this.masse) * (dt / 1000) - (this.vy * k);
-			this.x += this.vx * dt;
-			this.y += this.vy * dt;
+			this.x += this.vx * (dt);
+			this.y += this.vy * (dt);
 			if (this.y <= 0) {
 				this.vy = Math.abs(this.vy);
 				this.y = 0;
 			}
+			var x =  this.x / 20;
+			var y =  -(this.y / 20) + 200;
+			this.element.animate({
+				left: x,
+				top: y
+			}, dt, function() {
+				self.element.removeClass("hidden");
+				if (next) {
+					next();
+				}
+			});
 		}
 	};
-
-
-	function atomicMove(particule, next) {
-		particule.move();
-		var x =  particule.x / 20;
-		var y =  -(particule.y / 20) + 200;
-		particule.element.animate({
-			left: x,
-			top: y
-		}, dt, function() {
-			particule.element.removeClass("hidden");
-			if (next) {
-				next();
-			}
-		});
-	}
-
 
 	var app = angular.module('mainApp', []);
 
 
 	app.controller('MainCtrl', [ '$scope', function($scope) {
-		var p1 = new Particule($('#p1'), 0.1, 10, -20, 0, 4000);
+		var p1 = new Particule($('#p1'), 0.05, 10, -20, 0, 4000);
 		var p2 = new Particule($('#p2'), 0.1, 10, 10, 4000, 3000);
 
 		var move = function() {
 
 			return new Promise(function(fulfill, reject) {
 				$scope.$apply();
-				atomicMove(p1, fulfill);
-				atomicMove(p2, null);
+				p1.move(fulfill);
+				p2.move();
 			});
 		};
 
