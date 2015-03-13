31d1e89738cf1c3c2be58d4f07905870517635bc
Juan Trochez <juan.trochez95@gmail.com>
2015-03-13 13:03:47 +0100

    hash

--- 16_factory/app.js
+++ 16_factory/app.js
@@ -1,19 +1,22 @@
 (function() {
+	'use strict';
 	var app = angular.module('myApp', []);
-	app.value('myInfo', {
-		version: '1.2.4',
-		author: 'JLG'
+	app.value('hash', { name: 'md5', hash: function(n) { return new Hashes.MD5().hex(n); }
 	});
-
-	app.factory('myAppDetails', [ 'myInfo', function(myInfo) {
+	app.factory('passwordService', [ 'hash', '$log', function(hash, $log) {
 		var result = {};
-		result.loadedTime = new Date();
-		result.getVersion = function() { return myInfo.version; };
+		result.hash = function(login, password) {
+			var r = hash.hash(login + password);
+			$log.debug('hash = ', r);
+			return r;
+		}
 		return result;
 	}]);
+	app.controller('MyController', [ 'passwordService', 'hash', '$scope', function(passwordService, hash, $scope) {
+		$scope.hash = hash;
+		$scope.$watch('password + login', function() {
+			$scope.passwordHash = passwordService.hash($scope.login, $scope.password);
+		});
 
-	app.controller('MyController', [ 'myAppDetails', function(myAppDetails) {
-		this.version = myAppDetails.getVersion();
-		this.loadedTime = myAppDetails.loadedTime;
 	}]);
 })();
