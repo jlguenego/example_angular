80d1511ad9b4849f321512c7d6e03c5dad8abf1e
Juan Trochez <juan.trochez95@gmail.com>
2015-03-13 13:55:32 +0100

    service hash

--- 17_service/app.js
+++ 17_service/app.js
@@ -1,20 +1,21 @@
 (function() {
+	'use strict';
 	var app = angular.module('myApp', []);
-	app.value('myInfo', {
-		version: '1.2.4',
-		author: 'JLG'
+	app.value('hash', { name: 'md5', hash: function(n) { return new Hashes.MD5().hex(n); }
 	});
+	app.service('passwordService', [ 'hash', '$log', function PasswordService(hash, $log) {
+		var self = this;
+		this.hash = function(login, password) {
+			var r = hash.hash(login + password);
+			$log.debug('hash = ', r, self);
+			return r;
+		}
+	}]);
+	app.controller('MyController', [ 'passwordService', 'hash', '$scope', function(passwordService, hash, $scope) {
+		$scope.hash = hash;
+		$scope.$watch('password + login', function() {
+			$scope.passwordHash = passwordService.hash($scope.login, $scope.password);
+		});
 
-	function Person(myinfo) {
-		this.sayHello = function() {
-			return 'Hello I am ' + this.name;
-		};
-		this.name = myinfo.author;
-	}
-
-	app.service('myPerson', [ 'myInfo', Person ]);
-
-	app.controller('MyController', [ 'myPerson', function(myPerson) {
-		this.hello = myPerson.sayHello();
 	}]);
 })();
