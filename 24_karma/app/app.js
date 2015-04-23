a4bfaa0bac6a3f77ee328bdcb094b679273d0432
Juan Trochez <juan.trochez95@gmail.com>
2015-04-23 17:02:37 +0200

    creation de app

--- 24_karma/app/app.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	var app = angular.module('myApp', []);
-
-	app.controller('myController', [ function() {
-		this.message = "Hello World!";
-	}]);
-})();
--- 25_protractor/app/app.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	var app = angular.module('myApp', []);
-
-	app.controller('myController', [ '$scope', function($scope) {
-		$scope.message = "Hello World!";
-	}]);
-})();
