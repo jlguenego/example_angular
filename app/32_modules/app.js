2f81d173f6bd3dc18a59c2a11aca5ed8cf7afee7
Juan Trochez <juan.trochez95@gmail.com>
2015-06-18 17:33:47 +0200

    fastclick

--- app/32_modules/app.js
+++ app/32_modules/app.js
@@ -1,7 +1,7 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('myApp', ['jlgMenu']);
+	var app = angular.module('myApp', ['jlgMenu', 'jlgCheckbox']);
 
 
 })();
---
+++ app/32_modules/jlg-checkbox/jlg-checkbox.js
@@ -0,0 +1,55 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('jlgCheckbox', []);
+
+	app.run(function() {
+		FastClick.attach(document.body);
+	});
+
+	app.directive('jlgCheckbox', ['$injector', function($injector) {
+		var $compile = $injector.get('$compile');
+
+		return {
+			restrict: 'EAC',
+			link: function(scope, element, attrs, ctrl) {
+				console.log('scope', scope);
+				console.log('attrs', attrs);
+				var ngModel = attrs['ngModel'];
+				console.log('ngModel', ngModel);
+				var div = angular.element('<div ng-click="switch()" class="jlg-checkbox-div"><span></span></div>');
+				$compile(div)(scope);
+				element.after(div);
+				var duration = 100;
+
+				var animate = function(element, from, to, done) {
+					element.css({
+						position: 'absolute',
+						top: 0,
+						left: from,
+						display: 'block'
+					});
+
+					jQuery(element).velocity({
+						left: to
+					}, duration, done);
+
+					return function(cancel) {
+						if(cancel) {
+							element.stop();
+						}
+					};
+				};
+
+				scope.switch = function() {
+					console.log('switch');
+					console.log('scope[ngModel]', scope[ngModel]);
+					scope[ngModel] = !scope[ngModel];
+					var from = scope[ngModel] ? 0 : 20;
+					var to = scope[ngModel] ? 20 : 0;
+					animate(angular.element('.jlg-checkbox-div span'), from, to);
+				};
+			}
+		};
+	}]);
+})();
