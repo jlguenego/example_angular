53039cead9b283d1f3a94f0f266408fa9bf78b66
Juan Trochez <juan.trochez95@gmail.com>
2015-06-18 18:03:40 +0200

    ng model

--- app/32_modules/jlg-checkbox/jlg-checkbox.js
+++ app/32_modules/jlg-checkbox/jlg-checkbox.js
@@ -17,7 +17,7 @@
 				console.log('attrs', attrs);
 				var ngModel = attrs['ngModel'];
 				console.log('ngModel', ngModel);
-				var div = angular.element('<div ng-click="switch()" class="jlg-checkbox-div"><span></span></div>');
+				var div = angular.element('<div ng-click="switch(\'' + ngModel + '\')" class="jlg-checkbox-div ' + ngModel + '"><span></span></div>');
 				$compile(div)(scope);
 				element.after(div);
 				var duration = 100;
@@ -40,13 +40,13 @@
 					};
 				};
 
-				scope.switch = function() {
+				scope.switch = function(ngModel) {
 					console.log('switch');
-					console.log('scope[ngModel]', scope[ngModel]);
+					console.log('scope[' + ngModel + ']', scope[ngModel]);
 					scope[ngModel] = !scope[ngModel];
 					var from = scope[ngModel] ? left : right;
 					var to = scope[ngModel] ? right : left;
-					var span = angular.element('.jlg-checkbox-div span');
+					var span = angular.element('.jlg-checkbox-div.' + ngModel + ' span');
 					if (scope[ngModel]) {
 						animate(span, from, to, function() {
 							span.addClass('glyphicon glyphicon-ok');
