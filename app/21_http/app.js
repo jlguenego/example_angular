7e43d5592ba7ae9d2944a1d12b1d1f2fb33ce973
Julien TRONEL <rashe25@gmail.com>
2015-11-05 17:10:36 +0100

    ok

--- app/21_http/app.js
+++ app/21_http/app.js
@@ -4,8 +4,11 @@
 	var app = angular.module('myApp', []);
 
 	app.controller('MyController', ['$scope', '$http', function($scope, $http) {
-		$http.get('content.json').success(function(data) {
-			$scope.content = data.content;
+		$http.get('content.json').then(function(response) {
+			$scope.content = response.data.content;
+		}).catch(function(error) {
+			$scope.content = '';
+			console.log('Error', error);
 		});
 	}]);
 })();
