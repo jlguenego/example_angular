aa7bb921785b6bea8d02e36ffdec7255aa347738
Julien TRONEL <rashe25@gmail.com>
2015-11-06 11:18:09 +0100

    ok

--- app/20_injector/app.js
+++ app/20_injector/app.js
@@ -6,10 +6,12 @@
 	app.value('s2', { value: 0 });
 
 	app.controller('MyController', ['$scope', '$injector', function($scope, $injector) {
+		var $log = $injector.get('$log');
 		$scope.myService = 's1';
 		$scope.increment = function() {
 			var service = $injector.get($scope.myService);
 			service.value++;
+			$log.debug('value', service.value);
 		};
 
 		$scope.value = function() {
