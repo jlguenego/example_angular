1fd579d62fb2cf6a9e66c6c2c903b5c9acec82a2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-06-12 17:21:50 +0200

    upgrade jquery to 3.0.0 (for upgrading karma)

--- app/24_karma2/test/karma.conf.js
+++ app/24_karma2/test/karma.conf.js
@@ -6,7 +6,7 @@ module.exports = function(config) {
 		basePath: '../../../',
 
 		files: [
-		'bower_components/jquery/jquery.js',
+		'bower_components/jquery/dist/jquery.js',
 		'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
 		'bower_components/angular/angular.js',
 		'bower_components/angular-route/angular-route.js',
@@ -22,17 +22,6 @@ module.exports = function(config) {
 		frameworks: ['jasmine'],
 
 		browsers: ['Chrome'],
-
-		plugins: [
-		'karma-chrome-launcher',
-		'karma-firefox-launcher',
-		'karma-jasmine'
-		],
-
-		junitReporter: {
-			outputFile: 'test_out/unit.xml',
-			suite: 'unit'
-		}
 
 	});
 };
