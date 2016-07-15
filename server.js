61b78962d895542a45f8d231d344884b74a6bc45
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-07-15 19:23:54 +0200

    added express for global server

---
+++ server.js
@@ -0,0 +1,20 @@
+(function() {
+	'use strict';
+
+	var express = require('express'); // charge ExpressJS
+	var serveIndex = require('serve-index');
+
+	var app = express();
+	
+	app.use(express.static('.'));
+	app.use(serveIndex('.', {icons: true}));
+
+	app.use(function(req, res, next) {
+		console.log('404: Page not Found', req.url);
+		next();
+	});
+
+	app.listen(8000, function() {
+		console.log('server started on port 8000');
+	});
+})();
