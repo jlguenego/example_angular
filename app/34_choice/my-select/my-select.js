b446546b47fda415ede9b5aeccb27d6e72f983f6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-02-12 19:09:18 +0100

    34

--- app/34_choice/my-select/my-select.js
+++ app/34_choice/my-select/my-select.js
@@ -11,7 +11,7 @@
 			choices: '<',
 			placeholder: '@',
 		},
-		controller: function MySelect($scope, $element, $document, $window, $compile) {
+		controller: function MySelect($scope, $element, $document, $window, $compile, $timeout) {
 			console.log('MySelect', arguments);
 			var ctrl = this;
 			var localView = $document.find('local-view');
@@ -20,6 +20,8 @@
 			
 			ctrl.start = function() {
 				ctrl.editMode = true;
+				$timeout(function() { $element.find('input')[0].focus(); }, 0);
+				
 				localView.html('<my-select-local c="$ctrl"></my-select-local');
 				$compile(localView.contents())($scope);
 			};
@@ -32,6 +34,28 @@
 				ctrl.value = choice;
 				ctrl.stop();
 			};
+			
+			ctrl.selectFirst = function() {
+				if (!ctrl.value) {
+					ctrl.value = ctrl.filteredChoices[0];
+				}
+				ctrl.stop();
+			};
+			
+			ctrl.filter = function(value) {
+				if (ctrl.pattern && value.toLowerCase().indexOf(ctrl.pattern.toLowerCase()) == -1) {
+					return false;
+				}
+				if (value === ctrl.value) {
+					return false;
+				}
+				return true;
+			}
+			
+			$scope.$watchGroup(['$ctrl.pattern', '$ctrl.value'], function() {
+				console.log('refresh filteredChoices');
+				ctrl.filteredChoices = ctrl.choices.filter(ctrl.filter).slice(0,20);
+			}); 
 		}
 	});
 	
