60603bdece6bfbca9e31e1dc5646be5b79dcbbce
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-03-18 11:06:58 +0100

    refactor: check 1-10

--- app/03_javascript_callback/main.js
+++ app/03_javascript_callback/main.js
@@ -8,7 +8,7 @@
 		}, 1000);
 	}
 	// eslint-disable-next-line no-redeclare
-	for (var i = 0; i < 10; i++) {
+	for (var i = 0; i < 12; i++) {
 		(function(j) {
 			setTimeout(function() {
 				console.log('i=' + j);
