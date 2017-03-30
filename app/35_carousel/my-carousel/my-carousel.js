22fea4cfc8fce1ef31eec29e493537b41ebfc1dc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-03-30 20:08:44 +0200

    add: carousel working

--- app/35_carousel/my-carousel/my-carousel.js
+++ app/35_carousel/my-carousel/my-carousel.js
@@ -9,18 +9,40 @@
 			var ctrl = this;
 			var currentSlideId = 0;
 			var slides = $element.find('slide');
+			var total = slides.length;
 			console.log('slides', slides);
 
 			ctrl.goto = function(id) {
+				console.log('goto', id);
 				slides.addClass('hidden');
+				slides.removeClass('visible');
 				var currentSlide = slides.eq(id);
 				currentSlide.removeClass('hidden');
 				currentSlide.addClass('visible');
 			};
 
+			ctrl.prev = function() {
+				console.log('prev');
+				currentSlideId--;
+				if (currentSlideId < 0) {
+					currentSlideId = total - 1;
+				}
+				ctrl.goto(currentSlideId);
+			};
+
+			ctrl.next = function() {
+				console.log('next');
+				currentSlideId++;
+				if (currentSlideId > (total - 1)) {
+					currentSlideId = 0;
+				}
+				ctrl.goto(currentSlideId);
+			};
+
 			ctrl.addLayout = function(id) {
 				$templateRequest('./my-carousel/tmpl/my-carousel.html').then(function(html) {
 					$element.append(html);
+					$compile($element.contents())($scope);
 				});
 			};
 
@@ -40,8 +62,12 @@
 			backgroundImage: '@',
 		},
 		controller: function SlideCtrl($element) {
+			'ngInject';
 			console.log('SlideCtrl', arguments);
 			var ctrl = this;
+			ctrl.$onInit = function() {
+				$element.css('background-image', 'url("' + ctrl.backgroundImage + '")');
+			};
 
 
 		}
