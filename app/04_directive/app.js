983221132a3f9466973c578b3343afe98b4f97a1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-05 16:27:50 +0200

    add: 04 star wars

--- app/04_directive/app.js
+++ app/04_directive/app.js
@@ -1,13 +1,12 @@
-(function() {
+(function () {
 	'use strict';
 
 	var app = angular.module('myApp', []);
 
-	app.directive('myFirstDirective', function() {
+	app.directive('myFirstDirective', function () {
 		return {
 			restrict: 'E',
-			templateUrl: 'my_template.html',
-			transclude: true
+			templateUrl: 'my_template.html'
 		};
 	});
 })();
