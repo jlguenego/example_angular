6d28e961c1f195587c04f026b3effa726d9759fe
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-05 20:12:51 +0200

    css: watch 08

--- app/08_watch/app.js
+++ app/08_watch/app.js
@@ -3,10 +3,32 @@
 
 	var app = angular.module('myApp', []);
 
-	app.controller('MyController', ['$scope', function($scope) {
-			$scope.lastUpdate = undefined;
-			$scope.$watch('name', function() {
-				$scope.lastUpdate = new Date();
-			});
-		}]);
+	app.controller('MyController', ['$scope', '$element', '$timeout', function($scope, $element, $timeout) {
+		var ctrl = this;
+		$scope.lastUpdate = new Date();
+		$scope.$watch('name', function() {
+			ctrl.update();
+		});
+
+		// This code uses the promises. Go to example 23 to see more about promises.
+		ctrl.update = function() {
+			var until = new Date();
+			$element.find('clock').addClass('updating');
+
+			function increment(until) {
+				$timeout(function() {
+					$scope.lastUpdate = new Date($scope.lastUpdate.getTime() + 1000);
+				}, 100).then(function() {
+					if ($scope.lastUpdate < until) {
+						return increment(until);
+					}
+					$element.find('clock').removeClass('updating');
+				});
+			}
+			increment(until);
+		};
+
+
+
+	}]);
 })();
