f3e98eb210a3ccfb6e2bad8ba010ea98ca828f64
Maïté THOMIAS <maite.thomias@gmail.com>
2017-06-06 15:41:34 +0200

    add: localstorage

--- app/09_form_checking/app.js
+++ app/09_form_checking/app.js
@@ -4,12 +4,21 @@
 	var app = angular.module('mainApp', []);
 
 	app.controller('MainCtrl', function() {
+		this.object = {};
+		this.comments = [];
+		var comments = JSON.parse(localStorage.getItem('09_form_checking'));
+		if (comments && comments.constructor === Array) {
+			this.comments = comments;
+		}
+		this.add = function() {
+			this.comments.push(this.object);
 			this.object = {};
-			this.list = [];
+			localStorage.setItem('09_form_checking', JSON.stringify(this.comments));
+		};
 
-			this.add = function() {
-				this.list.push(this.object);
-				this.object = {};
-			};
-		});
+		this.deleteAll = function() {
+			this.comments = [];
+			localStorage.removeItem('09_form_checking');
+		};
+	});
 })();
