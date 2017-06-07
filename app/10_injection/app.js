6ee96dd90903bb0c9e19dde30aa677a949678620
Maïté THOMIAS <maite.thomias@gmail.com>
2017-06-07 14:21:23 +0200

    add: css

--- app/10_injection/app.js
+++ app/10_injection/app.js
@@ -3,19 +3,74 @@
 
 	var myModule = angular.module('myModule', []);
 
-	myModule.service('hello', ['$window', function($window) {
-		return {
-			welcome: function(name) {
-				$window.alert('Hello ' + name + '!');
-			}
+	// implicit annotation (cannot support minification)
+	myModule.service('hello', function($log, $window) {
+		this.sayHello = function(name) {
+			$log.info('Hello ' + name + '!');
+			$window.alert('Hello ' + name + '!');
+		};
+	});
+
+	// inline array annotation
+	myModule.service('goodbye', ['$log', '$window', function($log, $window) {
+		this.sayGoodbye = function(name) {
+			$log.info('Goodbye ' + name + '!');
+			$window.alert('Goodbye ' + name + '!');
 		};
 	}]);
 
-	var app = angular.module('myApp', ['myModule']);
-	app.controller('MyController', ['$scope', 'hello', function($scope, hello) {
-		$scope.sayHello = function(name) {
-			hello.welcome(name);
-			console.log(name);
+	// $inject annotation
+	var helloAgain = function($log, $window) {
+		this.sayHelloAgain = function(name) {
+			$log.info('Hello again ' + name + '!');
+			$window.alert('Hello again ' + name + '!');
 		};
-	}]);
+	};
+	helloAgain.$inject = ['$log', '$window'];
+
+	myModule.service('helloAgain', helloAgain);
+
+	// ngInject annotation (need ng-annotate plugin)
+	myModule.service('goodbyeAgain', function($log, $window) {
+		'ngInject';
+		this.sayGoodbyeAgain = function(name) {
+			$log.info('Goodbye again ' + name + '!');
+			$window.alert('Goodbye again ' + name + '!');
+		};
+	});
+
+
+	// module app
+	var app = angular.module('myApp', ['myModule']);
+	app.controller('MyController', [
+		'$scope',
+		'hello',
+		'goodbye',
+		'helloAgain',
+		'goodbyeAgain',
+		function($scope, hello, goodbye, helloAgain, goodbyeAgain) {
+			$scope.sayHello = hello.sayHello;
+			$scope.sayHelloAgain = helloAgain.sayHelloAgain;
+			$scope.sayGoodbye = goodbye.sayGoodbye;
+			$scope.sayGoodbyeAgain = goodbyeAgain.sayGoodbyeAgain;
+		}
+	]);
 })();
+
+// small reminder about dependency injections.
+
+var myConsole = {
+	log: function(str) {
+		var node = document.createTextNode('log: ' + str);
+		var para = document.createElement("p");
+		para.appendChild(node);
+		document.body.appendChild(para);
+	}
+};
+
+function bonjour(console, str) {
+	console.log('hello ' + str);
+}
+
+bonjour(myConsole, 'Maite');
+bonjour(console, 'Jean-Louis');
