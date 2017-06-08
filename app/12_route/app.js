2651833045cdd65b233178ac12068436673fc3af
Maïté THOMIAS <maite.thomias@gmail.com>
2017-06-08 10:00:39 +0200

    add: css js12

--- app/12_route/app.js
+++ app/12_route/app.js
@@ -1,6 +1,8 @@
 (function() {
 	'use strict';
 
+	var img = 'http://businessasmission.com/wp-content/uploads/2015/04/above-the-clouds-845x321.jpeg';
+
 	var app = angular.module('myApp', ['ngRoute']);
 	app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 		$locationProvider
@@ -22,6 +24,16 @@
 				redirectTo: '/'
 			});
 	}]);
+
+	// just for the sunrise. Nothing to do with the SPA
+	app.run(function($document, $timeout) {
+		'ngInject';
+		angular.element('<img/>').attr('src', img).on('load', function() {
+			angular.element(this).remove();
+			angular.element($document[0].body).removeClass('is-loading');
+		});
+	});
+
 	app.controller('MessageController', ['$routeParams', function($routeParams) {
 		this.message = $routeParams.msg;
 	}]);
