ef069aab4e75843e254c89e6b7e4d2883c335de2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-09 17:41:28 +0200

    add: jsbeautify

--- app/15_value/app.js
+++ app/15_value/app.js
@@ -6,26 +6,12 @@
 		compute: function(n) {
 			return new Hashes[this.name]().hex(n);
 		},
-		algos: [{
-			name: 'MD5',
-			value: 'MD5'
-		},
-		{
-			name: 'SHA1',
-			value: 'SHA1'
-		},
-		{
-			name: 'SHA256',
-			value: 'SHA256'
-		},
-		{
-			name: 'SHA512',
-			value: 'SHA512'
-		},
-		{
-			name: 'RIPEMD-160',
-			value: 'RMD160'
-		}]
+		algos: [{ name: 'MD5', value: 'MD5' },
+			{ name: 'SHA1', value: 'SHA1' },
+			{ name: 'SHA256', value: 'SHA256' },
+			{ name: 'SHA512', value: 'SHA512' },
+			{ name: 'RIPEMD-160', value: 'RMD160' }
+		]
 	});
 
 	app.controller('MyController', ['hash', '$scope', function(hash, $scope) {
