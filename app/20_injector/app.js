28c8f5cd832fcdbf44f81cbf2400a16c8465ab09
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-06-10 19:44:59 +0200

    add: css filter

--- app/20_injector/app.js
+++ app/20_injector/app.js
@@ -2,21 +2,42 @@
 	'use strict';
 
 	var app = angular.module('myApp', []);
-	app.value('s1', {value: 0});
-	app.value('s2', {value: 0});
 
-	app.controller('MyController', ['$scope', '$injector', function($scope, $injector) {
+	app.value('grayscale', { value: 0 });
+	app.value('blur', { value: 0 });
+
+	app.controller('MyController', function MyController($scope, $injector) {
+		'ngInject';
+		var ctrl = this;
 		var $log = $injector.get('$log');
-		$scope.myService = 's1';
-		$scope.increment = function() {
-			var service = $injector.get($scope.myService);
+		ctrl.myService = 'blur';
+		ctrl.increment = function() {
+			var service = $injector.get(ctrl.myService);
 			service.value++;
-			$log.debug('value', service.value);
+			$log.info('value', service.value);
+		};
+		ctrl.decrement = function() {
+			var service = $injector.get(ctrl.myService);
+			service.value--;
+			$log.info('value', service.value);
 		};
 
-		$scope.value = function() {
-			var service = $injector.get($scope.myService);
+		ctrl.getValue = function() {
+			var service = $injector.get(ctrl.myService);
+			$log.info('service', service);
 			return service.value;
+
 		};
-	}]);
+
+		ctrl.grayscale = $injector.get('grayscale');
+		ctrl.blur = $injector.get('blur');
+
+		$scope.$watch('$ctrl', function() {
+			ctrl.style = {
+				filter: 'blur(' + ctrl.blur.value + 'px) grayscale(' + (ctrl.grayscale.value * 10) + '%)'
+			}
+		}, true);
+
+
+	});
 })();
